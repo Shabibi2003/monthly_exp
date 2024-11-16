426a3b4a96c95bf1feec5b18fdd3b2901063c932
Yousuf Shabibi <145971672+Shabibi2003@users.noreply.github.com>
2024-11-16 22:27:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,31 +3,31 @@ import sqlite3
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def init_db():
-    # Connect to the database file 'expenses.db'
-    conn = sqlite3.connect("expenses.db")
-    cursor = conn.cursor()
+# def init_db():
+#     # Connect to the database file 'expenses.db'
+#     conn = sqlite3.connect("expenses.db")
+#     cursor = conn.cursor()
 
-    # Create a table to store expenses if it doesn't exist
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS expenses (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            date TEXT NOT NULL,
-            category TEXT NOT NULL,
-            description TEXT,
-            amount REAL NOT NULL,
-            transaction_type TEXT NOT NULL
-        )
-    ''')
+#     # Create a table to store expenses if it doesn't exist
+#     cursor.execute('''
+#         CREATE TABLE IF NOT EXISTS expenses (
+#             id INTEGER PRIMARY KEY AUTOINCREMENT,
+#             date TEXT NOT NULL,
+#             category TEXT NOT NULL,
+#             description TEXT,
+#             amount REAL NOT NULL,
+#             transaction_type TEXT NOT NULL
+#         )
+#     ''')
 
-    # Save changes and close the connection
-    conn.commit()
-    conn.close()
+#     # Save changes and close the connection
+#     conn.commit()
+#     conn.close()
 
-# Run the init_db function to create the database table
-if __name__ == "__main__":
-    init_db()
-    print("Database initialized and table created successfully!")
+# # Run the init_db function to create the database table
+# if __name__ == "__main__":
+#     init_db()
+#     print("Database initialized and table created successfully!")
 
 # Initialize the Database Connection
 def init_connection():
