3001c39fd17d92bb4873491f32dfc30ed3608887
Yousuf Shabibi <shabibiyousuf@gmail.com>
2025-05-01 12:32:26 +0530

    updated

--- app.py
+++ app.py
@@ -329,20 +329,20 @@ with tab2:
         col1, col2, col3, col4 = st.columns(4)  # Add a new column for monthly savings
         
         with col1:
-            st.markdown('<div class="metric-card">', unsafe_allow_html=True)
+            st.markdown('<div class="card">', unsafe_allow_html=True)
             # Exclude monthly savings from total income
             total_in = transactions_df[(transactions_df["transaction_type"] == "Cash In") & (transactions_df["sub_category"] != "Monthly Savings")]["amount"].sum()
             st.metric("Total Income", f"₹{total_in:,.2f}")
             st.markdown('</div>', unsafe_allow_html=True)
             
         with col2:
-            st.markdown('<div class="metric-card">', unsafe_allow_html=True)
+            st.markdown('<div class="card">', unsafe_allow_html=True)
             total_out = transactions_df[transactions_df["transaction_type"] == "Cash Out"]["amount"].sum()
             st.metric("Total Expenses", f"₹{total_out:,.2f}")
             st.markdown('</div>', unsafe_allow_html=True)
             
         with col3:
-            st.markdown('<div class="metric-card">', unsafe_allow_html=True)
+            st.markdown('<div class="card">', unsafe_allow_html=True)
             # Calculate balance without monthly savings
             balance = total_in - total_out
             st.metric("Balance", f"₹{balance:,.2f}")
