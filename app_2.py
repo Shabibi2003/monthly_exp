df48c711ace96a3a9a557d95490904072bb409cf
Yousuf Shabibi <shabibiyousuf@gmail.com>
2025-05-13 16:23:11 +0530

    about

--- app_2.py
+++ app_2.py
@@ -347,18 +347,7 @@ if st.button("ℹ️ About", key="about_button", help="Learn how to use the app"
     # Add back button
     if st.button("Back to Dashboard", key="back_button"):
         st.rerun()
-    st.markdown("<br><br><br><br><br><br><br><br><br><br>", unsafe_allow_html=True)
-    # st.markdown("<br>", unsafe_allow_html=True)
-    # st.markdown("<br>", unsafe_allow_html=True)
-    # st.markdown("<br>", unsafe_allow_html=True)
-    # st.markdown("<br>", unsafe_allow_html=True)
-    # st.markdown("<br>", unsafe_allow_html=True)
-    # st.markdown("<br>", unsafe_allow_html=True)
-    # st.markdown("<br>", unsafe_allow_html=True)
-    # st.markdown("<br>", unsafe_allow_html=True)
-
-
-
+    st.markdown("<br><br><br><br><br><br><br><br><br><br><br><br><br>", unsafe_allow_html=True)
 
 
 # Add gap between header and analytics boxes
